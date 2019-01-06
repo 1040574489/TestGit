31a6217b94d41087480012e234cbb0b90d0a9e43
eclipse_user <eclipse_user@1040.com>
2019-01-06 16:12:32 +0800

    modify--hot_fix

--- src/main/java/com/abing/git/Apple2.java
+++ src/main/java/com/abing/git/Apple2.java
@@ -3,6 +3,6 @@ package com.abing.git;
 public class Apple2 {
 	public static void main(String[] args) {
 		System.out.print("start....left");
-		System.out.print("start....hot_fix");
+		System.out.print("start....hot_fix111");
 	}
 }
