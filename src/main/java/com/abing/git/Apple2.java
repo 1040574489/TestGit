096f2025fd58ef9c91ccb1cae12e6d908b6b2c5d
1040574489 <1040574489@qq.com>
2019-01-08 12:24:32 +0800

    2hot_fix

--- src/main/java/com/abing/git/Apple2.java
+++ src/main/java/com/abing/git/Apple2.java
@@ -4,5 +4,6 @@ public class Apple2 {
 	public static void main(String[] args) {
 		System.out.print("start....left");
 		System.out.print("start....hot_fix111");
+		System.out.print("start....2hot_fix");
 	}
 }
