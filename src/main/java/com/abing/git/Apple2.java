ea8999aac4b4d7238cc44df9fb0125a04e94ceb5
1040574489 <1040574489@qq.com>
2019-01-13 20:52:58 +0800

    jenkins002

--- src/main/java/com/abing/git/Apple2.java
+++ src/main/java/com/abing/git/Apple2.java
@@ -1,15 +1,15 @@
 package com.abing.git;
 
 public class Apple2 {
-	public static void main(String[] args) {
-		System.out.print("start....right");
-		System.out.print("start....left");
-		System.out.print("start....hot_fix111");
-		System.out.print("start....2hot_fix");
-		System.out.print("start....2hot_fix2");
-		System.out.print("start....git2hot_fix2");
-		System.out.print("start....ideamodify");
-		System.out.print("start....jenkinsByGit");
+    public static void main(String[] args) {
+        System.out.print("start....right");
+        System.out.print("start....left");
+        System.out.print("start....hot_fix111");
+        System.out.print("start....2hot_fix");
+        System.out.print("start....2hot_fix2");
+        System.out.print("start....git2hot_fix2");
+        System.out.print("start....ideamodify");
+        System.out.print("start....jenkinsByGit");
         System.out.print("start....jenkinsByGit002");
-	}
+    }
 }
